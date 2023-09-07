38af907d94f51d6871f1b37e7e0466f3ad10d975
oddaaron00 <caseygoodings@gmail.com>
2023-09-08 00:05:06 +0100

    Projects page coming soon message

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,13 @@
+import Subtitle from "../components/Subtitle";
 import Title from "../components/Title";
 
 export default function Projects() {
   return (
     <main className="text-center">
-      <Title text="Projects" extraClasses="p-2" />
+      <div className="p-2">
+        <Title text="Projects" extraClasses="pb-1" />
+        <Subtitle text="Coming soon" extraClasses="pt-1" />
+      </div>
     </main>
   );
 }
