3307c2b17cc4d3e9e8430ba753cdce0e3b396b75
oddaaron00 <caseygoodings@gmail.com>
2023-09-09 20:00:26 +0100

    Fix header styling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import HeaderLink from "./HeaderLink";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 grid grid-flow-col justify-stretch bg-white text-center text-lg">
+    <header className="sticky top-0 flex items-center bg-white text-center text-lg">
       <HeaderLink href="#about" label="About Me" />
       <HeaderLink href="#projects" label="Projects" />
       <HeaderLink href="#cv" label="CV" />
