654638236c9ce3a8678849fa1317d70eaaa3c753
oddaaron00 <caseygoodings@gmail.com>
2023-09-09 19:51:46 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  i18n: {
+    locales: ["en-GB"],
+    defaultLocale: "en-GB",
+  },
+  poweredByHeader: false,
+};
 
 module.exports = nextConfig;
