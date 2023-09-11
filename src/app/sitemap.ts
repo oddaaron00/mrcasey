3009566663e59a5033a7557fb120b87cbec87090
oddaaron00 <caseygoodings@gmail.com>
2023-09-11 20:20:46 +0100

    Correct URL in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://mrcasey.com",
+      url: "https://www.mrcasey.com/",
       lastModified: new Date(),
     },
   ];
