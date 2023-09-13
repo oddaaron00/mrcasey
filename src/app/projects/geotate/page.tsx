e150165f079c43f34cc9e77b911f0fed9e9a08c5
oddaaron00 <caseygoodings@gmail.com>
2023-09-13 12:12:59 +0100

    Basic metadata

--- src/app/projects/geotate/page.tsx
+++ src/app/projects/geotate/page.tsx
@@ -1,6 +1,13 @@
 import Title from "@/app/components/Title";
 import Loading from "@/app/loading";
+import { Metadata } from "next";
 import dynamic from "next/dynamic";
+
+export const metadata: Metadata = {
+  title: "mrcasey | Geotate",
+  description: "Casey Goodings – Software Engineer and Developer | Geotate",
+  robots: "index, nofollow",
+};
 
 const App = dynamic(() => import("./app/App"), {
   ssr: false,
