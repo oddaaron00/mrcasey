a3ccd954d25f1e3cfada5d6ab2ee974fd362bdcc
oddaaron00 <caseygoodings@gmail.com>
2023-09-14 01:51:51 +0100

    Styling

--- src/app/projects/geotate/app/components/ScoreScreen.tsx
+++ src/app/projects/geotate/app/components/ScoreScreen.tsx
@@ -8,35 +8,36 @@ interface IScoreScreen {
 
 export default function ScoreScreen({ scores, cumulativeScore }: IScoreScreen) {
   return (
-    <section>
-      <table className="w-screen">
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Country</th>
-            <th>Points</th>
-          </tr>
-        </thead>
-        <tbody>
-          {scores.map(({ city, country, code, points }) => (
-            <tr key={`${city}, ${country}`}>
-              <td className="w-1/3">{city as string}</td>
-              <td className="my-2 flex items-center justify-center">
-                <span className="mx-2">{country as string}</span>
-                <div className="relative h-8 w-16">
-                  <Image
-                    alt={`Flag of ${country}`}
-                    src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${code}.svg`}
-                    layout="fill"
-                    objectFit="contain"
-                  />
-                </div>
-              </td>
-              <td className="w-1/3">{points as string}</td>
+    <section className="w-screen">
+      <div className="flex justify-center p-16">
+        <table className="w-3/5">
+          <thead>
+            <tr>
+              <th className="text-left">Location</th>
+              <th></th>
+              <th>Points</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {scores.map(({ city, country, code, points }) => (
+              <tr key={`${city}, ${country}`}>
+                <td className="w-2/3 text-left">{`${city}, ${country}`}</td>
+                <td className="my-1 flex justify-center">
+                  <div className="relative h-8 w-16">
+                    <Image
+                      alt={`Flag of ${country}`}
+                      src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${code}.svg`}
+                      layout="fill"
+                      objectFit="contain"
+                    />
+                  </div>
+                </td>
+                <td className="w-1/6">{points as string}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <Subtitle
         text={`You scored ${cumulativeScore} points.`}
         extraClasses="m-16"
