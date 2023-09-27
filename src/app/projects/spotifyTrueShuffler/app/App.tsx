49d2e7a7d60547ea88ac2023fefed2d8b65e2927
oddaaron00 <caseygoodings@gmail.com>
2023-09-27 03:15:28 +0100

    Generify redirectUri generator

--- src/app/projects/spotifyTrueShuffler/app/App.tsx
+++ src/app/projects/spotifyTrueShuffler/app/App.tsx
@@ -8,8 +8,13 @@ export default function App() {
     const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
     if (!clientId) return;
     const scope = "user-read-email";
-    const redirectUri =
-      "http://localhost:3000/projects/spotifyTrueShuffler/callback";
+    const redirectUri = new URL(
+      "/projects/spotifyTrueShuffler/callback",
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://www.mrcasey.com",
+    ).toString();
+    console.log("redirectUri:", redirectUri);
     const state = "TEST_STATE";
 
     const fullUrl = `https://accounts.spotify.com/authorize?response_type=${responseType}&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}&state=${state}`;
