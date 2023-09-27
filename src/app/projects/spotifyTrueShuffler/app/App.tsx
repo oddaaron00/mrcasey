15630217d4f3cde0c99a11c91221e1837ca9d6e8
oddaaron00 <caseygoodings@gmail.com>
2023-09-27 03:22:01 +0100

    Extract SignInForm

--- src/app/projects/spotifyTrueShuffler/app/App.tsx
+++ src/app/projects/spotifyTrueShuffler/app/App.tsx
@@ -1,33 +1,11 @@
-import { randomBytes } from "crypto";
-import { redirect } from "next/navigation";
+import SignInForm from "./components/SignInForm";
 
-export default function App() {
-  const spotifySignIn = async (formData: FormData) => {
-    "use server";
-
-    const responseType = "code";
-    const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
-    if (!clientId) return;
-    const scope = "user-read-email";
-    const redirectUri = new URL(
-      "/projects/spotifyTrueShuffler/callback",
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000"
-        : "https://www.mrcasey.com",
-    ).toString();
-    console.log("redirectUri:", redirectUri);
-    const state = randomBytes(20).toString("hex");
-
-    const fullUrl = `https://accounts.spotify.com/authorize?response_type=${responseType}&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}&state=${state}`;
-
-    redirect(fullUrl);
-  };
-
+const App = () => {
   return (
     <article className="flex flex-col items-center">
-      <form action={spotifySignIn}>
-        <button type="submit">Sign In To Spotify</button>
-      </form>
+      <SignInForm />
     </article>
   );
-}
+};
+
+export default App;
---
+++ src/app/projects/spotifyTrueShuffler/app/components/SignInForm.tsx
@@ -0,0 +1,33 @@
+import { randomBytes } from "crypto";
+import { redirect } from "next/navigation";
+
+const SignInForm = () => {
+  const spotifySignIn = async () => {
+    "use server";
+
+    const responseType = "code";
+    const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
+    if (!clientId) return;
+    const scope = "user-read-email";
+    const redirectUri = new URL(
+      "/projects/spotifyTrueShuffler/callback",
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://www.mrcasey.com",
+    ).toString();
+    console.log("redirectUri:", redirectUri);
+    const state = randomBytes(20).toString("hex");
+
+    const fullUrl = `https://accounts.spotify.com/authorize?response_type=${responseType}&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}&state=${state}`;
+
+    redirect(fullUrl);
+  };
+
+  return (
+    <form action={spotifySignIn}>
+      <button type="submit">Sign In To Spotify</button>
+    </form>
+  );
+};
+
+export default SignInForm;
