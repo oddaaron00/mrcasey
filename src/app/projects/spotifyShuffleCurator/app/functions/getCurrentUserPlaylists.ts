3e9a690004aa243fc82050ab7ff662ac4b8ebcd0
oddaaron00 <caseygoodings@gmail.com>
2023-09-27 07:52:52 +0100

    Sort playlist names

--- src/app/projects/spotifyShuffleCurator/app/functions/getCurrentUserPlaylists.ts
+++ src/app/projects/spotifyShuffleCurator/app/functions/getCurrentUserPlaylists.ts
@@ -11,6 +11,12 @@ const getCurrentUserPlaylists = (accessToken: string): Promise<Playlist[]> =>
     .then((json) => json.items)
     .then((items: Record<string, unknown>[]) =>
       items.map((item) => ({ id: item.id, name: item.name }) as Playlist),
-    );
+    )
+    .then((playlists) => {
+      playlists.sort((a, b) =>
+        a.name < b.name ? -1 : a.name > b.name ? 1 : 0,
+      );
+      return playlists;
+    });
 
 export default getCurrentUserPlaylists;
