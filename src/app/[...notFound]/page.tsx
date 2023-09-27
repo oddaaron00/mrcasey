93de5aec8671b6404c39d92e2290c069e58de289
oddaaron00 <caseygoodings@gmail.com>
2023-09-27 07:42:22 +0100

    Add Home link on 404 page

--- src/app/[...notFound]/page.tsx
+++ src/app/[...notFound]/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <main className="flex-1 flex-col text-center">
       <h1>Page Not Found</h1>
+      <Link href="/">Back to Home</Link>
     </main>
   );
 }
