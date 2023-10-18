1bd30e02b996bd88809d9e928fc81ed1774ae428
oddaaron00 <caseygoodings@gmail.com>
2023-10-18 16:14:52 +0100

    Allow description in header

--- src/app/projects/components/Header.tsx
+++ src/app/projects/components/Header.tsx
@@ -4,19 +4,23 @@ import Link from "next/link";
 interface IHeader {
   title: string;
   githubHref: string;
+  description?: string;
 }
 
-const Header: React.FC<IHeader> = ({ title, githubHref }) => (
-  <header className="sticky top-0 flex items-center justify-between bg-white px-2 pb-8 pt-6 text-center text-lg">
-    <div className="w-1/5">
-      <Link href="/">Home</Link>
+const Header: React.FC<IHeader> = ({ title, githubHref, description }) => (
+  <header className="sticky top-0 bg-white px-2 pb-2 pt-6 text-center">
+    <div className="flex items-center justify-between text-lg">
+      <div className="w-1/5">
+        <Link href="/">Home</Link>
+      </div>
+      <div className="w-3/5">
+        <Title text={title} />
+      </div>
+      <div className="w-1/5">
+        <GoToGithub href={githubHref} />
+      </div>
     </div>
-    <div className="w-3/5">
-      <Title text={title} />
-    </div>
-    <div className="w-1/5">
-      <GoToGithub href={githubHref} />
-    </div>
+    {description && <p className="py-4">{description}</p>}
   </header>
 );
 
--- src/app/projects/components/ProjectContainer.tsx
+++ src/app/projects/components/ProjectContainer.tsx
@@ -3,16 +3,18 @@ import { Header } from ".";
 interface IProjectContainer {
   title: string;
   githubHref: string;
+  description?: string;
   children: React.ReactNode;
 }
 
 const ProjectContainer: React.FC<IProjectContainer> = ({
   title,
   githubHref,
+  description,
   children,
 }) => (
   <>
-    <Header title={title} githubHref={githubHref} />
+    <Header title={title} githubHref={githubHref} description={description} />
     <main className="flex-1 flex-col pb-8 text-center">{children}</main>
   </>
 );
